208d9ff1d101fd9aa8ea65ccf02562427eac18df
Vaibhav Korde <tech.vaibhavk@gmail.com>
2024-02-21 08:35:30 +0530

    lamda with list

---
+++ src/com/lambda/ListWithLambda.java
@@ -0,0 +1,21 @@
+package com.lambda;
+
+import java.util.List;
+
+public class ListWithLambda {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> list = List.of(234,25,23,63,23,74,26,6);
+		
+		list.forEach(
+				
+				(n) -> System.out.println(n)
+				
+		);
+		
+		
+		
+	}
+
+}
