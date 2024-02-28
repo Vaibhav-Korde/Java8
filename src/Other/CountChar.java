b3398d6679fc30228a5cf680a67cbb9efcdb4445
Vaibhav Korde <tech.vaibhavk@gmail.com>
2024-02-28 09:29:04 +0530

    new logic added

--- src/Other/CountChar.java
+++ src/Other/CountChar.java
@@ -1,5 +1,6 @@
 package Other;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -8,9 +9,21 @@ public class CountChar {
 
 	public static void main(String[] args) {
 		String word = "AAABBB";
-		Map<String, Long> charCount = word.codePoints().mapToObj(Character::toString)
-		        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-		System.out.println(charCount);
+//		Map<String, Long> charCount = word.codePoints().mapToObj(Character::toString)
+//		        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+//		System.out.println(charCount);
+		
+		
+		
+		Map<String, Integer> charCount = new HashMap<>();
+	    for(String charr: word.split("")){
+	        Integer added = charCount.putIfAbsent(charr, 1);
+	        if(added != null)
+	            charCount.computeIfPresent(charr,(k,v) -> v+1);
+	    }
+
+	    System.out.println(charCount);
+		
 
 	}
 
